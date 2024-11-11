4d1391d5832df5e34f9ec11fd9e330e6f4033b51
0408petrus <petrus.marcellino15@students.usu.ac.id>
2024-11-11 12:38:42 +0700

    Feat Add Comment

--- js/single.js
+++ js/single.js
@@ -3,25 +3,51 @@ const email = document.getElementById("email");
 const comment = document.getElementById("comment");
 
 const form = document.querySelector(".userComment");
+const comments = document.querySelector(".list");
 
 form.addEventListener("submit", function (event) {
   event.preventDefault();
   addComment(userName.value, email.value, comment.value);
 });
 
-function addComment(userName, email, comment) {
-  if (userName !== "" && email !== "" && comment !== "") {
+// function add comment with name, email, and comment and push to comment list after that store it in local storage
+function addComment(name, email, comment) {
+  if (name !== "" && email !== "" && comment !== "") {
     const userComment = {
-      userName: userName,
+      id: Date.now(),
+      name: name,
       email: email,
       comment: comment,
     };
-
     comments.push(userComment);
     addToLocalStorage(comments);
-
+    renderComments(comments);
     userName.value = "";
     email.value = "";
     comment.value = "";
   }
+}
+
+// function to render given comments to screen
+function renderComments(comments) {
+  commentList.innerHTML = "";
+  comments.forEach(function (item) {
+    const li = document.createElement("li");
+    li.setAttribute("class", "item");
+    li.setAttribute("data-key", item.id);
+    const h3 = document.createElement("h3");
+    h3.textContent = item.name;
+    const p1 = document.createElement("p");
+    p1.textContent = item.email;
+    const p2 = document.createElement("p");
+    p2.textContent = item.comment;
+    li.append(h3, p1, p2);
+    commentList.append(li);
+  });
+}
+
+// function to add comments to local storage
+function addToLocalStorage(comments) {
+  localStorage.setItem("comments", JSON.stringify(comments));
+  renderComments(comments);
 }
